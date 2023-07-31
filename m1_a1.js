037bc3e64204e63946ec028ed20ac4d1d9580cfc
Md Ashad <mdashadnstu97@gmail.com>
2023-07-31 14:33:55 +0600

    Assignment 1: Write a function to calculate the sum of the first N natural numbers

---
+++ m1_a1.js
@@ -0,0 +1,13 @@
+const prompt = require("prompt-sync")();
+const number = parseInt(prompt("Enter a positive integer: "));
+
+let sum = 0;
+function sum_of_natural_numbers() {
+  for (let i = 1; i <= number; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
+sum_of_natural_numbers(number);
+console.log("The sum of natural numbers:", sum);
