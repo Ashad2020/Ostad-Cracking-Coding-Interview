82fde612af01b5bf699e51dee5d992b9ad4ca97a
Md Ashad <mdashadnstu97@gmail.com>
2023-08-14 17:50:43 +0600

    valid parentheses assignment

---
+++ Module 3/assignment.js
@@ -0,0 +1,26 @@
+function isValidParentheses(s) {
+  let stack = [];
+  let myMap = new Map();
+  myMap.set("(", ")");
+  myMap.set("{", "}");
+  myMap.set("[", "]");
+
+  for (let i = 0; i < s.length; i++) {
+    let char = s[i];
+    if (myMap.has(char)) {
+      stack.push(char);
+    } else {
+      if (stack.length === 0 || myMap.get(stack.pop()) !== char) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+}
+
+// Test cases
+console.log(isValidParentheses("()")); // true
+console.log(isValidParentheses("()[]{}")); // true
+console.log(isValidParentheses("(]")); // false
+console.log(isValidParentheses("([)]")); // false
+console.log(isValidParentheses("{[]}")); // true
